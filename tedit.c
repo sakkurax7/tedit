8936a6ec8351113414facc43c04039716e960a01
ejnAjaK3VgnnHBLk <charles.igel@s.stemk12.org>
2020-04-22 01:10:15 -0600

    added features

--- tedit.c
+++ tedit.c
@@ -4,16 +4,33 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 // Defines
+#define TEDIT_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
 
 // DATA
 
 struct editorConfig {
+	int cx, cy; // Cursor x and y position
+
 	int screenrows;
 	int screencols;
 
@@ -81,7 +98,7 @@ void enableRawMode()
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey()
+int editorReadKey()
 {
 	int nread;
 	char c;
@@ -89,6 +106,55 @@ char editorReadKey()
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
 	{
 		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+
+	if (c == '\x1b')
+	{
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '[')
+		{
+			if (seq[1] >= '0' && seq[1] <= '9') 
+			{
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') 
+				{
+					switch (seq[1])
+					{
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			} else {
+				switch (seq[1]) 
+				{
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
+			}
+		} else if (seq[0] == 'O') {
+			switch (seq[1]) 
+			{
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+		}
+
+		return '\x1b';
+	} else {
+		return c;
 	}
 
 	return c;
@@ -132,32 +198,121 @@ int getWindowSize(int *rows, int *cols)
 	}
 }
 
+// Append buffer 
+// Solves issue of flicering effect by having 1 large write
+// function instead of small ones.
+struct abuf {
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0};
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+
+	memcpy(&new[ab->len], s, len);
+
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab) { free(ab->b); }
+
 // Output
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
 	int i; 
 
 	for (i = 0; i < E.screenrows; i++)
 	{
-		write(STDOUT_FILENO, "~\r\n", 3);
+		if (i == E.screenrows / 3)
+		{
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+							"tEdit editor -- verison %s", TEDIT_VERSION);
+			if(welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			if (padding)
+			{
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		} else {
+			abAppend(ab, "~", 1);
+		}
+		
+		abAppend(ab, "~", 1);
+
+		abAppend(ab, "\x1b[K", 3);
+		if (i < E.screenrows - 1) 
+		{ 
+			abAppend(ab, "\r\n", 2);
+		}
 	}
 }
 
 void editorRefreshScreen()
 {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	struct abuf ab = ABUF_INIT;
 
-	editorDrawRows();
+	abAppend(&ab, "\x1b[?25l", 6);
+	abAppend(&ab, "\x1b[H", 3);
 
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	editorDrawRows(&ab);
+
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	abAppend(&ab, buf, strlen(buf));
+
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
 // Input
 
+void editorMoveCursor(int key)
+{
+	switch (key)
+	{
+		case ARROW_LEFT:
+			if (E.cx != 0)
+			{
+				E.cx--;
+			}
+			break;
+		case ARROW_RIGHT:
+			if (E.cx != E.screencols -1) 
+			{
+				E.cx++;
+			}
+			break;
+		case ARROW_UP:
+			if (E.cy != 0)
+			{
+				E.cy--;
+			}
+			break;
+		case ARROW_DOWN:
+			if (E.cy != E.screenrows - 1)
+			{
+				E.cy++;
+			}
+			break;
+	}
+}
+
 void editorProcessKeypress()
 {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c)
 	{
@@ -166,6 +321,30 @@ void editorProcessKeypress()
  			write(STDOUT_FILENO, "\x1b[H", 3); 
 			exit(0);
 			break;
+
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols -1;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while (times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+			break;
+
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
+			editorMoveCursor(c);
+			break;
 	}
 }
 
@@ -173,6 +352,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+	E.cx = 0;
+	E.cy = 0;
+	
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindwowSize");
 }
 
